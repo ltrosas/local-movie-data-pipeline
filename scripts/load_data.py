60014a0a2fcaace1ec37fa74b2c6cf1ea5b3ce04
ltrosas <lucasrosas98@hotmail.com>
2025-04-05 04:24:06 -0300

    oops

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,20 +1,19 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# Update with your macOS username
 DB_USER = os.getenv("DB_USER", "ltrosas")
-DB_PASS = os.getenv("DB_PASS")  # Will raise error if not set
+DB_PASS = os.getenv("DB_PASS")  # will raise error if not set!!!
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "movies")
 
-# Load CSV
+# load CSV
 df = pd.read_csv("../data/movies.csv")
 
-# Connect to PostgreSQL
+# connect
 engine = create_engine(f"postgresql://{DB_USER}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
-# Load to PostgreSQL
+# load to db
 df.to_sql("movies_raw", engine, if_exists="replace", index=False)
 
 print("Data successfully loaded into 'movies_raw'")
